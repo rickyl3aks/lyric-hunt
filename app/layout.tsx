7415ad507e64837832084be49db7f5444c114d10
Riccardo Consolandi <rconsolandi@gmail.com>
2023-09-02 13:39:57 +0200

    refactor: remove vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
 
 import Footer from "./components/footer/footer";
 
@@ -33,7 +32,6 @@ export default function RootLayout({
           {children}
         </div>
         <Footer />
-        <Analytics />
       </body>
     </html>
   );
